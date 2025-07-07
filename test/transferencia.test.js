207bcb0e4476d74f62e44b7f57cf2391739c3c48
Manoella Souza <100487940+manoellasouza@users.noreply.github.com>
2025-07-07 01:31:59 -0300

    Adicionando testes de transferência

---
+++ test/transferencia.test.js
@@ -0,0 +1,61 @@
+
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transferencias', () => {
+  describe('POST /transferencias', () => {
+    it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
+        // Capturar o token, do mesmo jeito que fizemos no login.test.js
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          'username': 'julio.lima',
+          'senha': '123456'
+        });     
+        
+        const token = respostaLogin.body.token
+      
+        const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        // para colocar uma variável dentro do bearer token, precisamos colocar ele entre crases
+        .set('Authorization',`Bearer ${token}`)
+        .send({
+            contaOrigem: 1,
+            contaDestino: 2,
+            valor: 11,
+            token: ''
+        });
+
+        expect(resposta.status).to.equal(201);
+    });
+
+    it('Deve retornar falha com 422 quando o valor da transferência for inferior a R$ 10,00', async () => {
+        // Capturar o token, do mesmo jeito que fizemos no login.test.js
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          'username': 'julio.lima',
+          'senha': '123456'
+        });     
+        
+        const token = respostaLogin.body.token
+      
+        const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        // para colocar uma variável dentro do bearer token, precisamos colocar ele entre crases
+        .set('Authorization',`Bearer ${token}`)
+        .send({
+            contaOrigem: 1,
+            contaDestino: 2,
+            valor: 7,
+            token: ''
+        });
+
+        expect(resposta.status).to.equal(422);
+        });
+  });
+});
