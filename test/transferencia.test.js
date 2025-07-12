d28c2d884f6cf4f44c73d5b1c5e49bf39f4ab34d
Manoella Souza <100487940+manoellasouza@users.noreply.github.com>
2025-07-12 16:36:42 -0300

    Adcionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao');
-
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferencias', () => {
   describe('POST /transferencias', () => {
@@ -14,35 +14,28 @@ describe('Transferencias', () => {
     })
 
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
-      
+        const bodyTransferencias = {...postTransferencias}
+
         const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         // para colocar uma variável dentro do bearer token, precisamos colocar ele entre crases
         .set('Authorization',`Bearer ${token}`)
-        .send({
-            contaOrigem: 1,
-            contaDestino: 2,
-            valor: 11,
-            token: ''
-        });
+        .send(bodyTransferencias);
 
         expect(resposta.status).to.equal(201);
     });
 
     it('Deve retornar falha com 422 quando o valor da transferência for inferior a R$ 10,00', async () => {
-      
+        const bodyTransferencias = {...postTransferencias}
+        bodyTransferencias.valor = 7
+
         const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         // para colocar uma variável dentro do bearer token, precisamos colocar ele entre crases
         .set('Authorization',`Bearer ${token}`)
-        .send({
-            contaOrigem: 1,
-            contaDestino: 2,
-            valor: 7,
-            token: ''
-        });
+        .send(bodyTransferencias);
 
         expect(resposta.status).to.equal(422);
         });
